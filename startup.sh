552176c6ae2dc08096561714e681376ed7949824
Jordi Santamaria Portoles <jordisantamaria1a@gmail.com>
2021-12-31 14:40:16 +0100

    install yarn

--- startup.sh
+++ startup.sh
@@ -56,6 +56,13 @@ eval "$(homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
 
+# Install yarn
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install yarn
+
 # Install docker
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
