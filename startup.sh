b45f55286943eb36eb8b75b0779f2a2923d7f36d
jordisantamaria <jordisantamaria1a@gmail.com>
2022-10-26 15:35:43 +0900

    to update docker-compose to last version

--- startup.sh
+++ startup.sh
@@ -92,8 +92,9 @@ sudo chmod 666 /var/run/docker.sock
 sudo usermod -aG docker ${USER}
 
 # Install docker-compose
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+export COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+sudo sh -c "curl -L http://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo sh -c "curl -L http://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo chown $(whoami):$(whoami) /var/run/docker.sock
 
