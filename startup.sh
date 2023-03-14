62be90e284d9404f1824efe3bb0a49ffdc157620
Jordi Santamaria Portoles <jordisantamaria1a@gmail.com>
2023-03-14 10:25:12 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -52,10 +52,10 @@ git config --global user.name jordisantamaria
 
 # Install brew
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(homebrew/bin/brew shellenv)"
-brew update --force --quiet
-chmod -R go-w "$(brew --prefix)/share/zsh"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
 
 # Install last version of node and npm
 
