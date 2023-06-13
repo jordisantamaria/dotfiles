fbe9ee1ec9fa3fb9ae7857b5d4d1392adcee5092
Jordi Santamaria Portoles <jordisantamaria1a@gmail.com>
2023-06-13 17:54:55 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -187,6 +187,19 @@ curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.zshrc
 nvm install v16.14.0
 
+# Install aws cli
+
+curl "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip"
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install
+sam --version
+
+# Install sam cli
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 # Install amplify
 npm install -g @aws-amplify/cli
 
